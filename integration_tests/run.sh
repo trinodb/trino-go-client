176ed797700cde86978b93583dd06d44ead80468
Jan Was <jan@was.net.pl>
2020-12-23 16:40:20 +0000

    Run tests with a timeout

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -5,36 +5,34 @@ LOCAL_PORT=8080
 # TODO update to trino after the release
 IMAGE_NAME=prestosql/presto
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
 function test_cleanup() {
-   docker rm -f $CONTAINER
+    docker rm -f "$CONTAINER"
 }
 
 trap test_cleanup EXIT
 
 function test_query() {
-	docker exec -t $CONTAINER bin/presto --server localhost:${LOCAL_PORT} --execute "$*"
+    docker exec -t "$CONTAINER" bin/presto --server localhost:${LOCAL_PORT} --execute "$*"
 }
 
 CONTAINER=$(docker run -v "$PWD/etc:/etc/presto" -p ${LOCAL_PORT}:${LOCAL_PORT} --rm -d $IMAGE_NAME)
 
 attempts=10
-while [ $attempts -gt 0 ]
-do
-	attempts=`expr $attempts - 1`
-	ready=`test_query "SHOW SESSION" | grep task_writer_count`
-	[ ! -z "$ready" ] && break
-	echo "waiting for trino..."
-	sleep 2
+while [ $attempts -gt 0 ]; do
+    attempts=$((attempts - 1))
+    ready=$(test_query "SHOW SESSION" | grep task_writer_count)
+    [ -n "$ready" ] && break
+    echo "waiting for trino..."
+    sleep 2
 done
 
-if [ $attempts -eq 0 ]
-then
-	echo "timed out waiting for trino"
-	exit 1
+if [ $attempts -eq 0 ]; then
+    echo "timed out waiting for trino"
+    exit 1
 fi
 
 PKG=../trino
 DSN=http://test@localhost:${LOCAL_PORT}
-go test -v -cover -coverprofile=coverage.out $PKG -trino_server_dsn=$DSN $*
+go test -v -race -timeout 10s -cover -coverprofile=coverage.out $PKG -trino_server_dsn=$DSN "$@"
